ffa20595273483f728a7188857177a45cdfb5a0f
Ankit Garg <dragonholy44@gmail.com>
2018-12-02 18:01:59 +0530

    added function to show current time in a formatted text

--- app/src/main/java/com/kotlindemo/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/kotlindemo/kotlindemo/MainActivity.kt
@@ -4,17 +4,40 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.Toast
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var btnClick: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("Hello, world!")
         btnClick = findViewById<Button>(R.id.btnClick)
         btnClick.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Clicked!", Toast.LENGTH_LONG).show()
+            showToast(timeNow())
         }
+    }
+
+    /**
+     * Shows a toast message.
+     *
+     *@param message The message to display as toast.
+     */
+    private fun showToast(message: String) {
+        Toast.makeText(this@MainActivity, message, Toast.LENGTH_LONG).show()
+    }
+
+    /**
+     * Returns current time in a string.
+     *
+     * @return Current time in a formatted string.
+     */
+    private fun timeNow(): String {
+        val simpleDateFormat = SimpleDateFormat("hh:mm aa",
+                Locale.getDefault())
+        return simpleDateFormat.format(Date())
     }
 }
